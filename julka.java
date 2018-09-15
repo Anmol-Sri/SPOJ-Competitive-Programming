d71ecd3e3995e72add7c94ddb06b6fa0e09fe8d3
Anmol-Sri <anmolsrirocks@gmail.com>
2018-09-16 02:25:23 +0530

    Initial commit

---
+++ julka.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.math.BigInteger;
+class julka
+{
+	public static void main(String [] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		for(int i=1;i<=10;i++)
+		{
+			BigInteger b=sc.nextBigInteger();
+			BigInteger m=sc.nextBigInteger();
+			BigInteger ans=b.subtract(m);
+			ans=ans.divide(BigInteger.valueOf(2));
+			System.out.println(ans.add(BigInteger.valueOf(2)));
+			System.out.println(ans);
+		}
+
+
+	}
+}
