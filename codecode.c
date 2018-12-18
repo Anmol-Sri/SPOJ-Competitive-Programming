f74b5e7da935967691b2c05fb799ebefa2b7e3a2
ANMOL SRIVASTAVA <anmolsrirocks@gmail.com>
2018-12-19 00:37:55 +0530

    Update codecode.c

--- codecode.c
+++ codecode.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
-
 int main(void) {
-
 	int t,x,y;
-
 	double pro;
-
-	scanf("%d",&t);
-	
+	scanf("%d",&t);	
 	while(t--){
-
 		scanf("%d",&x);
-
 		scanf("%d",&y);
-
 		pro=(x*y)+((100-x)*(100-y));
-
 		pro/=100;
-
 		printf("%.2f%\n",pro);
-
 	}
-
 	return 0;
-
 }
